80c6475854a6619b0220123fbb54aea5b1ec4d5b
Steven Campbell <stuckie@arcadebadgers.co.uk>
2021-02-04 11:44:52 +0000

    Minor fixes for latest emscripten

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-rm fishy.bc
-emmake make -f Makefile
-mv fishy fishy.bc
-emcc -O3 fishy.bc -o fishy.js -s USE_SDL=2 --embed-file data -s 'EXPORTED_FUNCTIONS=["_main", "_emscripten_fullscreen"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall"]'
-
+embuilder build sdl2 sdl2-mixer-ogg sdl2-image-png
+emmake make -f Makefile.emscripten
